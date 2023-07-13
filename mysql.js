40e174f71cd9c77e241be452b72f49c39fb13852
sam.ebersole@groenewold-it.solutions <samuelmebersole@gmail.com>
2023-07-14 00:02:50 +0200

    Added users table, createdAt and updatedAt now automatically generate

--- mysql.js
+++ mysql.js
@@ -7,9 +7,25 @@ const connection = mysql.createConnection({
   database: process.env.DATABASE_NAME,
 });
 
-exports.create = () => {};
-exports.read = (table) => new Promise((resolve, reject) => {
-  const query = `SELECT * FROM ${table}`;
+exports.create = (
+  table,
+  data,
+) => new Promise((resolve, reject) => {
+  const createdAt = new Date();
+  const row = { ...data, createdAt, updatedAt: createdAt };
+  const query = `INSERT INTO ${table} SET ?`;
+  connection.query(query, row, (error, results) => {
+    if (error) reject(error);
+    else resolve(results);
+  });
+});
+
+exports.read = (
+  table,
+  options,
+) => new Promise((resolve, reject) => {
+  const params = options.params || '';
+  const query = `SELECT * FROM ${table} ${params}`;
   connection.query(query, (error, results) => {
     if (error) reject(error);
     else resolve(results);
