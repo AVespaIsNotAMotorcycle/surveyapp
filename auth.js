284a45e3ed6860b2442310c76c99d6be89945ea7
sam.ebersole@groenewold-it.solutions <samuelmebersole@gmail.com>
2023-07-14 00:16:02 +0200

    auth.validateCredentials returns user id if valid

--- auth.js
+++ auth.js
@@ -1,7 +1,17 @@
-exports.validateCredentials = (username, password) => {
+const { read } = require('./mysql');
+
+exports.validateCredentials = async (username, password) => {
   if (username !== 'admin') return false;
   if (password !== 'admin') return false;
-  return true;
+  const { id } = await read(
+    'users',
+    {
+      exactlyOne: true,
+      params: `WHERE username="${username}"`,
+    },
+  );
+  console.log(`validateToken(${username}) => ${id}`);
+  return id;
 };
 
 exports.validateToken = () => {
